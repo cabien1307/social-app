36c94102d2137f173fa4accb0adb5727035aa808
CaBien <cabien13072000@gmail.com>
2021-07-30 20:13:41 +0700

    Social app chatbox-realtime

---
+++ client/src/context/AuthAction.js
@@ -0,0 +1,25 @@
+export const LoginStart = (userCredentials) => ({
+    type: "LOGIN_START",
+
+})
+
+export const LoginSuccee = (user) => ({
+    type: "LOGIN_SUCCESS",
+    payload: user
+    
+})
+
+export const LoginFailure = (err) => ({
+    type: "LOGIN_FAILURE",
+    payload: err
+})
+
+export const Follow = (userId) => ({
+    type: "FOLLOW",
+    payload: userId,
+  });
+  
+  export const Unfollow = (userId) => ({
+    type: "UNFOLLOW",
+    payload: userId,
+  });
